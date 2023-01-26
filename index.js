5a6e0a6d6b02379aec29b625758a47c76cd09727
George Washington <112564097+GeorgeWashingtonSaratoga@users.noreply.github.com>
2023-01-26 16:20:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+var c = document.getElementByID("gamecanvas");
+var ctx = c.getContext("2d");
 
+function main() { 
+  ctx.beginPath();
+  ctx.fillStyle="rgba(255,255,255)";
+  ctx.fillRect(0,0,512,512);
+  requestAnimationFrame(main);
+}
+
+requestAnimationFrame(main);
