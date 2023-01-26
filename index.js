473b37b2d51fb6f7634a6685646893cf28cafb7e
Zasharan2 <65794527+Zasharan2@users.noreply.github.com>
2023-01-26 16:58:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
+// get canvas & context
 var c = document.getElementById("gamecanvas");
 var ctx = c.getContext("2d");
 
+// get char image
+var charImg = document.getElementById("character");
+
+// define player class
+class Player {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+// init player
+var player = Player(16, 16)
+
 function main() { 
+  // draw background
   ctx.beginPath();
   ctx.fillStyle="rgba(255,255,255)";
   ctx.fillRect(0,0,512,512);
+  
+  // draw image
+  ctx.drawImage(charImg, 0, 0, 320, 320, player.x, player.y, 16, 16);
+  
+  // request aim frame
   window.requestAnimationFrame(main);
 }
 
+// request anim frame
 window.requestAnimationFrame(main);
