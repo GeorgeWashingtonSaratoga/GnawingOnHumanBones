2d77e5ef46186fcac83088473058978f59f6a746
George Washington <112564097+GeorgeWashingtonSaratoga@users.noreply.github.com>
2023-01-26 16:27:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function main() {
   ctx.beginPath();
   ctx.fillStyle="rgba(255,255,255)";
   ctx.fillRect(0,0,512,512);
-  requestAnimationFrame(main);
+  window.requestAnimationFrame(main);
 }
 
-requestAnimationFrame(main);
+window.requestAnimationFrame(main);
